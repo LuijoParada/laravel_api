b7f542173fe712a0ec41c3c29e5aa88d26467071
LuijoParada <lujoparca@gmail.com>
2024-06-15 15:20:57 -0400

    dsf

--- proyecto (prueba)/proyecto-prueba-api/app/Http/Controllers/Api/styleconfigController.php
+++ proyecto (prueba)/proyecto-prueba-api/app/Http/Controllers/Api/styleconfigController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Styleconfig;
 
 class styleconfigController extends Controller
 {
